2f55db92c23fe2e3798a54edbee75178c01c5150
Lasha Ilashvili <148916134+Lasha-Ilashvili@users.noreply.github.com>
2025-01-01 22:13:52 +0400

    Updating develop from main (#19)

--- src/main/resources/Fibonacci.kt
+++ src/main/resources/Fibonacci.kt
@@ -2,19 +2,19 @@ fun main() {
     print("Enter the value of N to find the Nth Fibonacci number: ")
     val n = readln().toInt()
 
-    if (n < 0) {
-        println("Fibonacci numbers are not defined for negative indices.")
+    if (n <= 0) {
+        println("Please enter a positive integer.")
     } else {
         var a = 0
         var b = 1
         var fib = 0
 
-        if (n == 0) {
+        if (n == 1) {
             fib = a
-        } else if (n == 1) {
+        } else if (n == 2) {
             fib = b
         } else {
-            var i = 2
+            var i = 3
             while (i <= n) {
                 fib = a + b
                 a = b
